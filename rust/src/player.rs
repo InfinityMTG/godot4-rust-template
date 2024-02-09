46daa27b97cf2708388567583cf5467f3fb6bdd8
Kieran Davies <attempt4032@protonmail.com>
2024-02-09 22:24:31 +0000

    Added all files relevant to the template

---
+++ rust/src/player.rs
@@ -0,0 +1,32 @@
+use godot::engine::Sprite2D;
+use godot::prelude::*;
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Player {
+    speed: f64,
+    angular_speed: f64,
+
+    base: Base<Sprite2D>,
+}
+use godot::engine::ISprite2D;
+
+#[godot_api]
+impl ISprite2D for Player {
+    fn init(base: Base<Sprite2D>) -> Self {
+        godot_print!("Hello, World!"); // Prints to the Godot console
+
+        Self {
+            speed: 400.0,
+            angular_speed: std::f64::consts::PI,
+            base,
+        }
+    }
+    fn physics_process(&mut self, delta: f64) {
+        // In GDScript, this would be:
+        // rotation += angular_speed * delta
+
+        let radians = (self.angular_speed * delta) as f32;
+        self.base_mut().rotate(radians);
+    }
+}

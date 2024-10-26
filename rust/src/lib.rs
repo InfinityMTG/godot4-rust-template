69619c8a98d702b7c2246b07f419169216e9b321
Kieran Davies <attempt4032@protonmail.com>
2024-10-26 17:15:00 +0100

    Refactoring - remove all features except including modules from the sources

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,8 @@
 use godot::prelude::*;
-struct MyExtension;
-
-#[gdextension]
-unsafe impl ExtensionLibrary for MyExtension {}
-use godot::classes::RandomNumberGenerator;
 
 mod player;
 
-pub fn _ready() {
-    let mut rng = RandomNumberGenerator::new_gd();
-    rng.randomize();
-    godot_print!("{:#?}", rng.randf_range(-10.0, 10.0));
-}
+struct DemoGame;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for DemoGame {}

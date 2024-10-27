4b6612dd00256d6c6f9e89ac863d3cb06b9e47e6
Kieran Davies <attempt4032@protonmail.com>
2024-10-27 09:59:42 +0000

    Implemented basic functionality from godot-rust book's Hello World

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,22 +1,31 @@
+use godot::classes::ISprite2D;
 use godot::classes::Sprite2D;
 use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
+#[export]
 struct Player {
     speed: f64,
     angular_speed: f64,
 
     base: Base<Sprite2D>,
 }
-use godot::classes::ISprite2D;
+#[godot_api]
+impl Player {
+    #[func]
+    pub fn start(&mut self, pos: Vector2) {
+        self.base_mut().set_global_position(pos);
+        self.base_mut().show();
+    }
+}
 
 #[godot_api]
 impl ISprite2D for Player {
     fn init(base: Base<Sprite2D>) -> Self {
         godot_print!("Hello, World!"); // Prints to the Godot console
 
-        Self {
+        Player {
             speed: 400.0,
             angular_speed: std::f64::consts::PI,
             base,
@@ -28,5 +37,9 @@ impl ISprite2D for Player {
 
         let radians = (self.angular_speed * delta) as f32;
         self.base_mut().rotate(radians);
+
+        let rotation = self.base().get_rotation();
+        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
+        self.base_mut().translate(velocity * delta as f32)
     }
 }
